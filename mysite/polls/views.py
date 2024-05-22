c405db4a70088fef728d294f3e58b9d8734734bb
Thomas AWOUNFOUET <thomas.awounfouet@gmail.com>
2024-05-22 17:21:04 +0200

    Writing your first Django app, part 1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+
+def index(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. 68addc82 is the polls index.")
